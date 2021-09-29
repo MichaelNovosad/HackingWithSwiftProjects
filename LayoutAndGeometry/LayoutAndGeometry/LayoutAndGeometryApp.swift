d6924ff5f49050dc1b5de0347eae689a13ff5f54
Michael Novosad <84763034+MichaelNovosad@users.noreply.github.com>
2021-09-29 10:01:30 +0300

    Added LayoutAndGeometry
    LayoutAndGeometry basics project

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Michael Novosad on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
