78dfe5b3551297d587e29915c1af5a846acda297
Michael Novosad <84763034+MichaelNovosad@users.noreply.github.com>
2021-09-29 10:02:21 +0300

    Added Flashzilla project
    New project added

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Michael Novosad on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
