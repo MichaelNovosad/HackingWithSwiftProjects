f1275976f6112289ea1831ef4394a1bc8519715e
Michael Novosad <84763034+MichaelNovosad@users.noreply.github.com>
2021-09-29 10:33:07 +0300

    Add Animations project
    This project was created to test and practice animations in Swift.

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Michael Novosad on 03.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
