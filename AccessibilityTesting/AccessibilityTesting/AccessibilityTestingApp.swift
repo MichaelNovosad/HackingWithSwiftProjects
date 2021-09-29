f57f533343defe862f518f8635c1e9d643eb7416
Michael Novosad <84763034+MichaelNovosad@users.noreply.github.com>
2021-09-29 10:04:21 +0300

    Added AccessibilityTesting project
    Added project where accessibility was described in easy way

---
+++ AccessibilityTesting/AccessibilityTesting/AccessibilityTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityTestingApp.swift
+//  AccessibilityTesting
+//
+//  Created by Michael Novosad on 08.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
