1493345cb0e9a8903f1b239c690fd57e1f3c1bde
Michael Novosad <84763034+MichaelNovosad@users.noreply.github.com>
2021-09-29 10:16:25 +0300

    Add Instafilter project
    Added one more project to repository

---
+++ Instafilter/Instafilter/ImagePicker.swift
@@ -0,0 +1,82 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Michael Novosad on 07.09.2021.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+}
+
+
+//
+//import SwiftUI
+//
+//struct ImagePicker: UIViewControllerRepresentable {
+//    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//        var parent: ImagePicker
+//        
+//        init(_ parent: ImagePicker) {
+//            self.parent = parent
+//        }
+//        
+//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//            if let uiImage = info[.originalImage] as? UIImage {
+//                parent.image = uiImage
+//            }
+//            
+//            parent.presentationMode.wrappedValue.dismiss()
+//        }
+//    }
+//    
+//    @Binding var image: UIImage?
+//    @Environment(\.presentationMode) var presentationMode
+//    
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//    
+//    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+//        let picker = UIImagePickerController()
+//        picker.delegate = context.coordinator
+//        return picker
+//    }
+//    
+//    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+//        
+//    }
+//}
+
---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Michael Novosad on 07.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
