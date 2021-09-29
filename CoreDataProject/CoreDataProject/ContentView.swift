0095f99e12f85b61a36fd1b4721ca26db31cdec6
Michael Novosad <84763034+MichaelNovosad@users.noreply.github.com>
2021-09-29 10:17:06 +0300

    Add CoreData project
    Added project with CoreData basics

---
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Michael Novosad on 03.09.2021.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext)  var moc
+
+    @FetchRequest(entity: Ship.entity(),
+        sortDescriptors: [], predicate: nil)
+    var ships: FetchedResults<Ship>
+
+    var body: some View {
+        VStack {
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            Button("Add Examples") {
+                            let ship1 = Ship(context: self.moc)
+                            ship1.name = "Enterprise"
+                            ship1.universe = "Star Trek"
+
+                            let ship2 = Ship(context: self.moc)
+                            ship2.name = "Defiant"
+                            ship2.universe = "Star Trek"
+
+                            let ship3 = Ship(context: self.moc)
+                            ship3.name = "Millennium Falcon"
+                            ship3.universe = "Star Wars"
+
+                            let ship4 = Ship(context: self.moc)
+                            ship4.name = "Executor"
+                            ship4.universe = "Star Wars"
+
+                            try? self.moc.save()
+                        }
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
